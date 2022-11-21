1def9e43f306e68834a361f1011ad1dab5331c58
Luis Fernandes <57709030+LSBSOLO@users.noreply.github.com>
2022-11-21 01:11:25 +0000

    added files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import {
+  FaFacebookF,
+  FaGit,
+  FaGithub,
+  FaTiktok,
+  FaTwitter,
+} from 'react-icons/fa';
+import ThemeToggle from './ThemeToggle';
+
+const Footer = () => {
+  return (
+    <div className='rounded-div mt-8 pt-8 text-primary'>
+    
+      <p className='text-center py-4'>Luis Fernandes</p>
+    </div>
+  );
+};
+
+export default Footer;
